b7e7e09e176418e8b15a6d4029e19bab56d6fd2e
Stephane Poirier <stephane.poirier@oifii.org>
2016-12-12 02:30:51 -0500

    first commit

---
+++ Release/spimidiprogramchange.bat
@@ -0,0 +1,9 @@
+rem spimidiprogramchange -d "Microsoft GS Wavetable SW Synth" 0 64
+rem spimidiprogramchange -d "Out To MIDI Yoke:  1" 0 64 
+rem spimidiprogramchange "Out To MIDI Yoke:  1" 0 64 
+rem
+rem spimidiprogramchange -d "Out To MIDI Yoke:  1" -c 0 64
+rem spimidiprogramchange -d "Microsoft GS Wavetable SW Synth" 0 64
+spimidiprogramchange -d "Out To MIDI Yoke:  1" 0 46
+
+
